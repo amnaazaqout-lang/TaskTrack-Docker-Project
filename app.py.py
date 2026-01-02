4b025a6c216f11f0d8aac5f90e3fa84e86cde22c
amnaazaqout-lang <amnaazaqout@gmail.com>
2026-01-02 19:02:44 +0300

    Add files via upload

---
+++ app.py.py
@@ -0,0 +1,7 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+@app.route('/')
+def get_tasks():
+    return jsonify({"tasks": ["Task 1", "Task 2", "Task 3"], "status": "success"})
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
